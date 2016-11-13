1997249edd6ef923c25b6a15769534f2cc63d9cf
Joseph Noel <jtnoel96@gmail.com>
2016-11-12 21:05:05 -0500

    Starting work on dynamic list of labs

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -4,7 +4,7 @@ var start 			= new Date(2014, 8-1, 25), // August 25, 2014
 		graduation 	= new Date(2018, 5-1, 20), // May 20, 2018
 		diff 				= 100 - ((graduation - today) / (graduation - start) * 100);
 // Add percentage to title over progress bar
-$("#grad_title").append(": " + Math.round(diff) + "% of the way there!");
+$("#grad_title").append(": " + Math.round(diff) + "%");
 // Initialize progressbar for scroll reveal to work
 $("#progressBar").progressbar({
   value: 1
@@ -22,3 +22,37 @@ var config = {
 window.sr = new ScrollReveal(config);
 // Make progressbar value appear on viewport
 sr.reveal("#progressBar > .ui-progressbar-value");
+
+var data = new XMLHttpRequest();
+data.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+		loadData(this);
+	}
+}
+data.open("GET", "resources/data.xml", true);
+data.send();
+
+function loadData(data) {
+	var xmlDoc = data.responseXML;
+	var xml = xmlDoc.documentElement.childNodes;
+	var labs = xmlDoc.getElementsByTagName("lab");
+	// var hws = xmlDoc.getElementsByTagName("hw");
+	var html = [];
+	for (i=0;i<labs.length;i++) {
+		var name, desc, url;
+		name = labs[i].getElementsByTagName("name")[0].childNodes[0].nodeValue;
+		desc = labs[i].getElementsByTagName("desc")[0].childNodes[0].nodeValue;
+		url = labs[i].getElementsByTagName("url")[0].childNodes[0].nodeValue;
+		html.push("<div class='col-md-6'><div class='portfolio-item'><a href='"+url+"'>"+name+"</a>"+desc+"</div></div>");
+	}
+	// for (i=0;i<hws.length;i++) {
+	// 	var name, desc, url;
+	// 	name = hws[i].getElementsByTagName("name")[0].childNodes[0].nodeValue;
+	// 	desc = hws[i].getElementsByTagName("desc")[0].childNodes[0].nodeValue;
+	// 	url = hws[i].getElementsByTagName("url")[0].childNodes[0].nodeValue;
+	// 	html.push("<li class='hw'><h1><a href='"+url+"'>"+name+"</a></h1><h2>"+desc+"</h2></li>");
+	// }
+	html.push("</ul>")
+
+	$("#labs").append(html.join(""));
+}
